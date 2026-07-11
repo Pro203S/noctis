fc033ed12963e24aed4fa195687409a937ac33d8
Pro203S <prosirea2501@gmail.com>
2026-07-12 06:52:43 +0900

    fix(creator): fixed type errors

--- src/noctis/class.ts
+++ src/noctis/class.ts
@@ -1,7 +1,17 @@
+import type { HTTPServer, ServerHandler } from "../server/creator";
+import createServer from "../server/creator";
 import type { NoctisConfig } from "../types";
 
 export default class Noctis {
+    private _server!: HTTPServer;
+    
     constructor(public config: NoctisConfig) {
-        
+        const handler: ServerHandler = (req, res) => {
+
+        };
+
+        this._server = createServer(handler, Boolean(config.https), config.https?.cert, config.https?.key);
     }
+
+    
 }
--- src/server/creator.ts
+++ src/server/creator.ts
@@ -1,9 +1,10 @@
 import http from 'node:http';
 import https from 'node:https';
 
-type Server = (req: http.IncomingMessage, res: http.OutgoingMessage) => any;
+export type HTTPServer = http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
+export type ServerHandler = (req: http.IncomingMessage, res: http.OutgoingMessage) => any;
 
-export default function createServer(handler: Server, secure?: boolean, key?: string, cert?: string) {
+export default function createServer(handler: ServerHandler, secure?: boolean, key?: string, cert?: string): HTTPServer {
     if (secure) {
         const server = https.createServer({
             key,
