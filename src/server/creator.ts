767a6560b57ba5e1cff8e32e245d0a11a8858fea
Pro203S <prosirea2501@gmail.com>
2026-07-12 06:57:33 +0900

    fix(creator.ts): fix type error

--- src/server/creator.ts
+++ src/server/creator.ts
@@ -2,7 +2,7 @@ import http from 'node:http';
 import https from 'node:https';
 
 export type HTTPServer = http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
-export type ServerHandler = (req: http.IncomingMessage, res: http.OutgoingMessage) => any;
+export type ServerHandler = http.RequestListener<typeof http.IncomingMessage, typeof http.ServerResponse>;
 
 export default function createServer(handler: ServerHandler, secure?: boolean, key?: string, cert?: string): HTTPServer {
     if (secure) {
