a840c19df911d6464edc5fdf9b939cb32790fe3d
Pro203S <prosirea2501@gmail.com>
2026-09-18 00:16:30 +0900

    fix: useInput이 키보드 외 입력을 받음

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,34 +1,50 @@
-import { useEffect, useState } from "react";
+import { PassThrough } from "node:stream";
 import { emitKeypressEvents, type Key } from "node:readline";
-
-emitKeypressEvents(process.stdin);
+import { useEffect, useState } from "react";
 
 type PressedKey = {
-    "name": string,
-    "shift": boolean,
-    "ctrl": boolean,
-    "alt": boolean,
-    "meta": boolean
+    name: string;
+    shift: boolean;
+    ctrl: boolean;
+    alt: boolean;
+    meta: boolean;
 };
+
+const keyboardStream = new PassThrough();
+
+emitKeypressEvents(keyboardStream);
 
 export default function useInput() {
     const [lastInputed, setLastInputed] = useState<PressedKey>();
 
     useEffect(() => {
-        const cb = (str: string | undefined, key: Key) => {
+        const onData = (data: Buffer) => {
+            const input = data.toString();
+
+            // SGR Mouse Event
+            if (/^\x1b\[<\d+;\d+;\d+[Mm]$/.test(input)) {
+                return;
+            }
+
+            keyboardStream.write(data);
+        };
+
+        const onKeypress = (str: string | undefined, key: Key) => {
             setLastInputed({
-                "name": key.name ?? str ?? "",
-                "shift": key.shift ?? false,
-                "ctrl": key.ctrl ?? false,
-                "alt": key.meta ?? false,
-                "meta": false,
+                name: key.name ?? str ?? "",
+                shift: key.shift ?? false,
+                ctrl: key.ctrl ?? false,
+                alt: key.meta ?? false,
+                meta: false,
             });
         };
 
-        process.stdin.on("keypress", cb);
+        process.stdin.on("data", onData);
+        keyboardStream.on("keypress", onKeypress);
 
         return () => {
-            process.stdin.off("keypress", cb);
+            process.stdin.off("data", onData);
+            keyboardStream.off("keypress", onKeypress);
         };
     }, []);
 
