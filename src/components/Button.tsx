943ba0791929092a4c9a60685eaef529f31f522b
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-09-18 17:58:24 +0900

    chore: creating button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,16 @@
+import { ViewStyle } from "@/render/styles.js";
+import View from "./View.js";
+
 type Props = {
     "label": string,
     "onClick"?: () => any;
-    
+    "style"?: ViewStyle
 }
 
-export default function Button() {
+export default function Button(props: Props) {
+    const { label, onClick, style } = props;
+    
+    return <View>
 
+    </View>;
 }
